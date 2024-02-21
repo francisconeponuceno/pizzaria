7125fc9d8ed5076596269ade14eeb3bb134dfbaf
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-02-21 13:18:45 -0300

    atualizacao

--- script.js
+++ script.js
@@ -7,14 +7,4 @@ menuToggle.addEventListener('click', () => {
     show = !show;
 });
 
-const botao = document.querySelector("button.pedir")
-const modal = document.querySelector(".abrir")
 
-function pedir() {
-    modal.classList.add('active')
-}
-
-function finalizar() {
-    modal.classList.remove('active')
-    window.alert('Seu pedido chegarar em 50 minutos')
-}
