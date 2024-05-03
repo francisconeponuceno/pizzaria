c3830adea157b91d9632091cf9ed1b12934dc96a
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-05-03 15:17:59 -0300

    atualizacao

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ var show = true;
 const menuContent = document.querySelector('.content');
 const menuToggle = menuContent.querySelector('.menu-toggle');
 
+const cardapio = document.getElementById('itens-cardapio');
+
 menuToggle.addEventListener('click', () => {
     menuContent.classList.toggle('on', show);
     show = !show;
 });
 
-const cart = document.getElementById('cart-btn')
-cart.addEventListener('click',function(){
-    console.log(cart)
-})
+const itens_cardapio = ( event) => {
+    resp = event.target.id
+    alert(resp)
+}
+
+cardapio.addEventListener('click', itens_cardapio);
